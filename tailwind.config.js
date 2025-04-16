28b814f1e41c18fac544035c0e64d7f820feeb2b
rntoq <rinato0804@gmail.com>
2025-04-16 13:23:35 +0600

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                inter: ['Inter', 'sans-serif'],
+            },
+            colors: {
+                greenPrimary: '#6cc56c', // Your primary green color
+                darkGray: '#333',
+            },
+        },
+    },
+    plugins: [],
+};
